c680b1834f312ff3d3f59530e5709a752a275a1c
malteriechmann <kontakt@malteriechmann.de>
2014-11-01 22:07:05 +0100

    header and navigation

--- assets/javascripts/jquery.main.js
+++ assets/javascripts/jquery.main.js
@@ -19,15 +19,38 @@ $(function() {
 /*
     init particle background
 */
-$('#start_simulate-wlan').particleground({
-    minSpeedX: 0.1,
-    maxSpeedX: 0.1,
-    minSpeedY: 0.1,
-    maxSpeedY: 0.1,
-    dotColor: '#ffffff',
-    lineColor: '#ffffff',
-    particleRadius: 10,
-    lineWidth: 1,
-    proximity: 100,
-    parallaxMultiplier: 20
+$(document).ready(function() {
+    $('#start_simulate-wlan').particleground({
+        minSpeedX: 0.1,
+        maxSpeedX: 0.1,
+        minSpeedY: 0.1,
+        maxSpeedY: 0.1,
+        dotColor: '#ffffff',
+        lineColor: '#ffffff',
+        particleRadius: 10,
+        lineWidth: 1,
+        proximity: 100,
+        parallaxMultiplier: 20
+    });
+});
+
+/*
+    sticky navigation
+*/
+$(document).ready(function() {
+    var stickyTop = $('#main-header').offset().top;
+    
+    var stickyNav = function(){
+        var scrollTop = $(window).scrollTop();
+             
+        if (scrollTop > stickyTop) { 
+            $('html').addClass('sticky');
+        } else {
+            $('html').removeClass('sticky'); 
+        }
+    };
+
+    $(window).scroll(function() {
+        stickyNav();
+    });
 });
