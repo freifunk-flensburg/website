f0a7bba9477fa4a634361ffca1dff186108ddde7
spralexx <rene.krick@gmx.de>
2014-11-01 23:31:35 +0100

    added contact info

--- content/contact.php
+++ content/contact.php
@@ -1,1 +1,16 @@
-<h2 class="main-caption">contact</h2>
+<h3 class="main-caption">Kontakt</h3>
+<hr class="contact_hr1">
+<div class="contact_content">
+	<p>
+		So erreichst du uns solltest du dich für Freifunk interessieren und weitere Fragen haben. 
+	</p><br>
+	
+	<div class="contact_adresse">
+	<a href="mailto:presse@freifunk-flensburg.de">presse@freifunk-flensburg.de</a> <br>
+	Telefon: <a href="tel:046157494595">046157494595</a>  <br>
+	Freifunk Flensburg <br>
+	c/o Nordlab e.V. <br>
+	Rude 27 <br>
+	24941 Flensburg <br>
+		</div>
+</div>
