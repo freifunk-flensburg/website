64cdd0e42b5346c183c96df20f17db20b79ee863
spralexx <rene.krick@gmx.de>
2014-11-02 00:08:49 +0100

    added questionmarks to faq.php

--- content/faq.php
+++ content/faq.php
@@ -1,36 +1,43 @@
 <h3 class="main-caption">FAQ</h3>
 <hr class="faq_hr1">
-<div class="faq_content">
-	<h4>Frage1</h4>
-	<p>warum das neuland?</p>
-	<h4>Antwort</h4>
-	<p>Das internet ist für uns alle neulandDas internet ist für uns alle neuland
-	Das internet ist für uns alle neulandDas internet ist für uns alle neulandDa
-	s internet ist für uns alle neulandDas internet ist für uns alle neulandDas 
-	internet ist für uns alle neulandDas internet ist für uns alle neulandDas interne
-	t ist für uns alle neulandDas internet ist für uns alle neulandDas intern
-	et ist für uns alle neuland</p>
-</div>
 
 	<div class="faq_content">
-	<h4>Frage2</h4>
-	<p>warum das neuland?</p>
-	<h4>Antwort</h4>
-	<p>Das internet ist für uns alle neuland</p>
-</div>
+	<img src="media/questionmark/questionmark.png"alt="">
+		<h4>Frage1</h4>
+		<p>warum das neuland?</p>
+		<h4>Antwort</h4>
+		<p>Das internet ist für uns alle neulandDas internet ist für uns alle neuland
+		Das internet ist für uns alle neulandDas internet ist für uns alle neulandDa
+		s internet ist für uns alle neulandDas internet ist für uns alle neulandDas 
+		internet ist für uns alle neulandDas internet ist für uns alle neulandDas interne
+		t ist für uns alle neulandDas internet ist für uns alle neulandDas intern
+		et ist für uns alle neuland</p>
+	</div>
 
 	<div class="faq_content">
-	<h4>Frage3</h4>
-	<p>warum das neuland?</p>
-	<h4>Antwort</h4>
-	<p>Das internet ist für uns alle neuland</p>
-</div>
+			<img src="media/questionmark/questionmark.png"alt="">
+
+			<h4>Frage2</h4>
+			<p>warum das neuland?</p>
+			<h4>Antwort</h4>
+			<p>Das internet ist für uns alle neuland</p>
+	</div>
 
 	<div class="faq_content">
-	<h4>Frage4</h4>
-	<p>warum das neuland?</p>
-	<h4>Antwort</h4>
-	<p>Das internet ist für uns alle neuland</p>
-</div>
+			<img src="media/questionmark/questionmark.png"alt="">
 
+			<h4>Frage3</h4>
+			<p>warum das neuland?</p>
+			<h4>Antwort</h4>
+			<p>Das internet ist für uns alle neuland</p>
+	</div>
+
+	<div class="faq_content">
+			<img src="media/questionmark/questionmark.png"alt="">
+
+			<h4>Frage4</h4>
+			<p>warum das neuland?</p>
+			<h4>Antwort</h4>
+			<p>Das internet ist für uns alle neuland</p>
+	</div>
 <hr class="faq_hr2">
