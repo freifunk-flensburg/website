86a77c3c37827deb2156ee8db2cf36bfc3f0febf
spralexx <rene.krick@gmx.de>
2014-11-01 21:23:41 +0100

    corected classes

--- content/faq.php
+++ content/faq.php
@@ -1,29 +1,29 @@
-<h2 class="faq_h2">FAQ</h2>
-<hr>
+<h3 class="faq_h2">FAQ</h3>
+<hr class="faq_hr1">
 <div class="faq_content">
-	<h3>Frage1</h3>
+	<h4>Frage1</h4>
 	<p>warum das neuland?</p>
-	<h3>Antwort</h3>
+	<h4>Antwort</h4>
 	<p>Das internet ist für uns alle neuland</p>
 </div>
 
 	<div class="faq_content">
-	<h3>Frage1</h3>
+	<h4>Frage1</h4>
 	<p>warum das neuland?</p>
-	<h3>Antwort</h3>
+	<h4>Antwort</h4>
 	<p>Das internet ist für uns alle neuland</p>
 </div>
 
 	<div class="faq_content">
-	<h3>Frage1</h3>
+	<h4>Frage1</h4>
 	<p>warum das neuland?</p>
-	<h3>Antwort</h3>
+	<h4>Antwort</h4>
 	<p>Das internet ist für uns alle neuland</p>
 </div>
 
 	<div class="faq_content">
-	<h3>Frage1</h3>
+	<h4>Frage1</h4>
 	<p>warum das neuland?</p>
-	<h3>Antwort</h3>
+	<h4>Antwort</h4>
 	<p>Das internet ist für uns alle neuland</p>
 </div>
