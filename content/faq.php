6e3800962390fd5e5506358b314e25fa18795b6b
spralexx <rene.krick@gmx.de>
2014-11-02 00:36:43 +0100

    changed faq layout and style.css

--- content/faq.php
+++ content/faq.php
@@ -1,43 +1,61 @@
 <h3 class="main-caption">FAQ</h3>
 <hr class="faq_hr1">
-
+<div class="faq_uneaven">
 	<div class="faq_content">
 	<img src="media/questionmark/questionmark.png"alt="">
-		<h4>Frage1</h4>
-		<p>warum das neuland?</p>
+		<h4>Frage 1</h4>
+		<p>Darf ich Freifunk einfach so benutzen ?</p>
 		<h4>Antwort</h4>
-		<p>Das internet ist für uns alle neulandDas internet ist für uns alle neuland
-		Das internet ist für uns alle neulandDas internet ist für uns alle neulandDa
-		s internet ist für uns alle neulandDas internet ist für uns alle neulandDas 
-		internet ist für uns alle neulandDas internet ist für uns alle neulandDas interne
-		t ist für uns alle neulandDas internet ist für uns alle neulandDas intern
-		et ist für uns alle neuland</p>
+		<p>Natürlich darfst du das, denn es ist ein Bürgernetz, dass allen gehört und an dem alle mitwirken. Daher würden wir uns freuen wenn auch du einen Knoten aufstellst.</p>
+	</div>
+
+
+	<div class="faq_content">
+			<img src="media/questionmark/questionmark.png"alt="">
+
+			<h4>Frage 3</h4>
+			<p>Wie kann ich Freifunk nutzen ?</p>
+			<h4>Antwort</h4>
+			<p>Sobald du im Empfangsbereich einer der Knoten bist kannst du Freifunk mit jedem WLAN-fähigem Gerät nutzen. Sei es Smartphone, Tablett oder PC. Sobald du ein WLAN angezeigt bekommst, welches den Namen flensburg.freifunk.net hat, kannst du dich dort ohne Passwort einloggen. Wo der nächste Knoten steht, siehst du auf unserer Karte.</p>
 	</div>
 
 	<div class="faq_content">
 			<img src="media/questionmark/questionmark.png"alt="">
 
-			<h4>Frage2</h4>
-			<p>warum das neuland?</p>
+			<h4>Frage 5</h4>
+			<p>Was ist wenn jemand etwas illegales über meinen Knoten herrunterlädt?</p>
 			<h4>Antwort</h4>
-			<p>Das internet ist für uns alle neuland</p>
+			<p>Du musst dir keine Sorgen machen, dass du Post von einem Anwalt bekommst. Wir sorgen mit technischen Mitteln dafür, das alles was über die Freifunk Knoten passiert nicht direkt, sondern über uns ins Internet geht. Der Freifunkverkehr wird verschlüsselt und über unsere Server ins Netz geleitet. So musst du dir keine Sorge um die Störerhaftung machen, falls jemand über deinen Knoten Illegales herrunterlädt.</p>
+	</div>
+
+</div>
+
+<div class="faq_eaven">
+	<div class="faq_content">
+			<img src="media/questionmark/questionmark.png"alt="">
+
+			<h4>Frage 2</h4>
+			<p>Was kostet Freifunk ?</p>
+			<h4>Antwort</h4>
+			<p>Wenn du Freifunk surfen willst kostet es dich nichts. Wenn Du selbst einen Freifunk-Zugang betreiben willst, entstehen lediglich Kosten für die Anschaffung eines Knoten, welche ab 18 Euro beginnen. Hinzuzüglich kommen Stromkosten, die sich jährlich auf etwa 5 Euro belaufen. Wenn Du zusätzlich auch noch deinen Internetanschluss teilen möchtest kommen natürlich auch noch die Kosten für diesen hinzu.</p>
 	</div>
 
 	<div class="faq_content">
 			<img src="media/questionmark/questionmark.png"alt="">
 
-			<h4>Frage3</h4>
-			<p>warum das neuland?</p>
+			<h4>Frage 4</h4>
+			<p>Ich habe keine Ahnung von Technik. Kann ich trotzdem einen Knoten betreiben?</p>
 			<h4>Antwort</h4>
-			<p>Das internet ist für uns alle neuland</p>
+			<p>Ja das kannst du. Entweder kannst du dir selber einen Knoten kaufen oder wir kaufen einen für Dich und helfen dir gern bei der Einrichtung oder richten diesen fertig ein, so dass Du deinen Knoten nur noch in die Steckdose stecken musst und gegebenenfalls mit deinem Router verbinden musst.</p>
 	</div>
 
 	<div class="faq_content">
 			<img src="media/questionmark/questionmark.png"alt="">
 
-			<h4>Frage4</h4>
-			<p>warum das neuland?</p>
+			<h4>Frage 6</h4>
+			<p>Ich habe keine Ahnung von Technik. Kann ich trotzdem einen Knoten betreiben?</p>
 			<h4>Antwort</h4>
-			<p>Das internet ist für uns alle neuland</p>
+			<p>Ja das kannst du. Entweder kannst du dir selber einen Knoten kaufen oder wir kaufen einen für Dich und helfen dir gern bei der Einrichtung oder richten diesen fertig ein, so dass Du deinen Knoten nur noch in die Steckdose stecken musst und gegebenenfalls mit deinem Router verbinden musst. </p>
 	</div>
+</div>
 <hr class="faq_hr2">
