2e3a27b1ac5dc08d40c8f1e37913ca5e5c6f9a6b
spralexx <rene.krick@gmx.de>
2014-11-01 21:45:19 +0100

    added boxes

--- content/faq.php
+++ content/faq.php
@@ -4,25 +4,30 @@
 	<h4>Frage1</h4>
 	<p>warum das neuland?</p>
 	<h4>Antwort</h4>
-	<p>Das internet ist für uns alle neuland</p>
+	<p>Das internet ist für uns alle neulandDas internet ist für uns alle neuland
+	Das internet ist für uns alle neulandDas internet ist für uns alle neulandDa
+	s internet ist für uns alle neulandDas internet ist für uns alle neulandDas 
+	internet ist für uns alle neulandDas internet ist für uns alle neulandDas interne
+	t ist für uns alle neulandDas internet ist für uns alle neulandDas intern
+	et ist für uns alle neuland</p>
 </div>
 
 	<div class="faq_content">
-	<h4>Frage1</h4>
+	<h4>Frage2</h4>
 	<p>warum das neuland?</p>
 	<h4>Antwort</h4>
 	<p>Das internet ist für uns alle neuland</p>
 </div>
 
 	<div class="faq_content">
-	<h4>Frage1</h4>
+	<h4>Frage3</h4>
 	<p>warum das neuland?</p>
 	<h4>Antwort</h4>
 	<p>Das internet ist für uns alle neuland</p>
 </div>
 
 	<div class="faq_content">
-	<h4>Frage1</h4>
+	<h4>Frage4</h4>
 	<p>warum das neuland?</p>
 	<h4>Antwort</h4>
 	<p>Das internet ist für uns alle neuland</p>
