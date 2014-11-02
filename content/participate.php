157017235bb25693612502e54ebc3d11392f8aef
malteriechmann <kontakt@malteriechmann.de>
2014-11-02 03:28:21 +0100

    first section for participation

--- content/participate.php
+++ content/participate.php
@@ -1,4 +1,4 @@
-<ul class="participate_list">
+<ul class="participate_list-choose-customer">
     <li class="active">
         <header>
             <h3>Anfänger</h3>
@@ -14,7 +14,28 @@
     <li>
         <header>
             <h3>Unterstützer</h3>
-            <p>Du findest das Projekte unterstützenswert und kannst materiell etwas beitragen?</p>
+            <p>Du findest das Projekt unterstützenswert und kannst finanziell etwas beitragen?</p>
         </header>
     </li>
 </ul>
+<article class="participate_show-customer active">
+    <h4>Du bist hier genau richtig</h4>
+    <p>Ein freies WLAN in Flensburg aufzubauen macht enorm Spaß, bedarf aber auch viel Arbeit. Wir brauchen viele Flensburger. Es wäre also toll, wenn du mitmachen möchtest.
+    <h4>Der erste Schritt, dein eigener Knoten</h4>
+    <p>Am einfachsten kannst du bei Freifunk Flensburg mitmachen, indem du selber einen Knoten aufstellst. Ein Knoten ist ein technisches Gerät (ein Router), auf dem unsere Freifunk-Software läuft. Den Knoten kannst du dir bei uns für ca. 20 Euro, komplett fertig eingerichtet, kaufen. Deinen Knoten brauchst du dann nur noch an den Strom und an das Internet anzuschließen und schon können du und andere über Freifunk surfen. So einfach ist das.</p>
+    <h4>Mach mehr und bringe dich ein</h4>
+    <p>Wir treffen uns jeden Montag mit einer Gruppen von Technikinteressierten und besprechen den weiteren Verlauf von Freifunk Flensburg. Komm doch einfach mal vorbei. Wir treffen uns beim Cowork Flensburg, Toosbüystraße 13, immer ab 18:00 Uhr.</p>
+    <h4>War etwas unverstänldich?</h4>
+    <p>Dann kontaktiere uns. Wo du uns genau findest, ob online oder offline, findest du <a href="#contact">unter Kontakt</a>.</p>
+</article>
+<article class="participate_show-customer">
+    - Wiki
+    - Freifunk.net
+    - Github
+    - Map
+    - Firmware herunterladen
+</article>
+<article class="participate_show-customer">
+    - Suche nach Räumen
+    - Spende
+</article>
