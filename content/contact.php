9dcdc4a2f8fa02ac13ec6c48208fa99db505b5b9
spralexx <rene.krick@gmx.de>
2014-11-02 02:22:12 +0100

    corrected boxes in contact.php

--- content/contact.php
+++ content/contact.php
@@ -1,14 +1,22 @@
-<div class="contact_content">
-	<p>
-		So erreichst du uns solltest du dich für Freifunk interessieren und weitere Fragen haben. 
-	</p><br>
-	
-	<div class="contact_adresse">
-	<a href="mailto:presse@freifunk-flensburg.de">presse@freifunk-flensburg.de</a> <br>
-	Telefon: <a href="tel:046157494595">046157494595</a>  <br>
-	Freifunk Flensburg <br>
-	c/o Nordlab e.V. <br>
-	Rude 27 <br>
-	24941 Flensburg <br>
+<div class="contact_left">
+	<div class="contact_content">
+		<p>
+			So erreichst du uns solltest du dich für Freifunk interessieren und weitere Fragen haben. 
+		</p><br>
+		
+		<div class="contact_adresse">
+			<a href="mailto:presse@freifunk-flensburg.de">presse@freifunk-flensburg.de</a> <br>
+			Telefon: <a href="tel:046157494595">046157494595</a>  <br>
+			Freifunk Flensburg <br>
+			c/o Nordlab e.V. <br>
+			Rude 27 <br>
+			24941 Flensburg <br>
 		</div>
+	</div>
+</div>
+<div class="contact_right">
+		<div class="contact_content">
+			<p>Freifunk Flensburg ist ein Projekt des nordlab e.V.. Interessierte sind gerne eingeladen bei unseren Treffen am Montag bei Cowork Flensburg, Toosbüystraße 13, ab 18:00 Uhr vorbei zu schauen.</p>
+		</div>
+	</div>
 </div>
