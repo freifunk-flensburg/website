e4c793d12835b45cfaaa5902d4b7210e0e41cd6a
malteriechmann <kontakt@malteriechmann.de>
2014-11-02 02:56:51 +0100

    logo and navigation fixed

--- content/about.php
+++ content/about.php
@@ -1,3 +1,9 @@
 <div class="about_main">
-<p>Freifunk Flensburg ist dein freies WLAN zum selber machen in Flensburg und Umgebung. Wir möchten möglichst vielen Menschen überall und jederzeit einen guten Zugang zum Internet bieten. Dafür arbeiten wir an einem Gemeinschaftsnetz, welches jede_r vergrößern und verbessern kann. Über Freifunk surfen ist nicht nur kostenlos, sondern auch unbegrenzt, kennwortlos und super einfach.</p>
+    <figure>
+        <img src="media/router.png">
+    </figure>
+    <h3>Was ist Freifunk Flensburg?</h3>
+    <p>Freifunk Flensburg ist dein freies WLAN zum selber machen in Flensburg und Umgebung.</p>
+    <p>Wir möchten möglichst vielen Menschen überall und jederzeit einen guten Zugang zum Internet bieten. Dafür arbeiten wir an einem Gemeinschaftsnetz, welches jede_r vergrößern und verbessern kann.</p>
+    <p>Über Freifunk surfen ist nicht nur kostenlos, sondern auch unbegrenzt, kennwortlos und super einfach.</p>
 </div>
