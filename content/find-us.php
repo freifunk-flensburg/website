83266fab21a219df44ceb279db2e6496123dcd9a
malteriechmann <kontakt@malteriechmann.de>
2014-11-02 04:55:58 +0100

    changed map and text order

--- content/find-us.php
+++ content/find-us.php
@@ -1,5 +1,5 @@
+<div id="map" class="find-us_map"></div>
 <h3>Ein schneller Überblick</h3>
 <p>Wir sind dabei unser Netz immer weiter auszubauen. Derzeit gibt es in Flensburg bereits <span id="count-nodes">an die 50</span> Knoten.</p>
 <h3>Du willst mehr Standorte auf dieser Karte sehen?</h3>
 <p>Du kannst deinen <a href="#participate">eigenen Knoten erstellen</a> und dabei helfen, Flensburg mit Freifunk zu versorgen.</p>
-<div id="map" class="map"></div>
