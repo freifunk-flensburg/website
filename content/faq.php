06f9db96e0764cb9080e2d541b0a6a5d8e593eea
malteriechmann <kontakt@malteriechmann.de>
2014-11-02 04:50:14 +0100

    improved faqs
    more semantic, no image any more, some css improvements, responsive web
    design

--- content/faq.php
+++ content/faq.php
@@ -1,58 +1,43 @@
-<div class="faq_uneaven">
-	<div class="faq_content">
-	<img src="media/questionmark/questionmark.png"alt="">
+<section class="faq_odd">
+	<article class="faq_content">
 		<h4>Frage</h4>
 		<p>Darf ich Freifunk einfach so benutzen ?</p>
 		<h4>Antwort</h4>
 		<p>Natürlich darfst du das, denn es ist ein Bürgernetz, dass allen gehört und an dem alle mitwirken. Daher würden wir uns freuen wenn auch du einen Knoten aufstellst.</p>
-	</div>
+	</article>
+	<article class="faq_content">
+		<h4>Frage</h4>
+		<p>Wie kann ich Freifunk nutzen ?</p>
+		<h4>Antwort</h4>
+		<p>Sobald du im Empfangsbereich einer der Knoten bist kannst du Freifunk mit jedem WLAN-fähigem Gerät nutzen. Sei es Smartphone, Tablett oder PC. Sobald du ein WLAN angezeigt bekommst, welches den Namen flensburg.freifunk.net hat, kannst du dich dort ohne Passwort einloggen. Wo der nächste Knoten steht, siehst du auf unserer Karte.</p>
+	</article>
 
+	<article class="faq_content">
+		<h4>Frage</h4>
+		<p>Was ist wenn jemand etwas illegales über meinen Knoten herrunterlädt?</p>
+		<h4>Antwort</h4>
+		<p>Du musst dir keine Sorgen machen, dass du Post von einem Anwalt bekommst. Wir sorgen mit technischen Mitteln dafür, das alles was über die Freifunk Knoten passiert nicht direkt, sondern über uns ins Internet geht. Der Freifunkverkehr wird verschlüsselt und über unsere Server ins Netz geleitet. So musst du dir keine Sorge um die Störerhaftung machen, falls jemand über deinen Knoten Illegales herrunterlädt.</p>
+	</article>
+</section>
 
-	<div class="faq_content">
-			<img src="media/questionmark/questionmark.png"alt="">
+<section class="faq_even">
+	<article class="faq_content">
+		<h4>Frage</h4>
+		<p>Was kostet Freifunk ?</p>
+		<h4>Antwort</h4>
+		<p>Wenn du Freifunk surfen willst kostet es dich nichts. Wenn Du selbst einen Freifunk-Zugang betreiben willst, entstehen lediglich Kosten für die Anschaffung eines Knoten, welche ab 18 Euro beginnen. Hinzuzüglich kommen Stromkosten, die sich jährlich auf etwa 5 Euro belaufen. Wenn Du zusätzlich auch noch deinen Internetanschluss teilen möchtest kommen natürlich auch noch die Kosten für diesen hinzu.</p>
+	</article>
+	<article class="faq_content">
+		<h4>Frage</h4>
+		<p>Ich habe keine Ahnung von Technik. Kann ich trotzdem einen Knoten betreiben?</p>
+		<h4>Antwort</h4>
+		<p>Ja das kannst du. Entweder kannst du dir selber einen Knoten kaufen oder wir kaufen einen für Dich und helfen dir gern bei der Einrichtung oder richten diesen fertig ein, so dass Du deinen Knoten nur noch in die Steckdose stecken musst und gegebenenfalls mit deinem Router verbinden musst.</p>
+	</article>
 
-			<h4>Frage</h4>
-			<p>Wie kann ich Freifunk nutzen ?</p>
-			<h4>Antwort</h4>
-			<p>Sobald du im Empfangsbereich einer der Knoten bist kannst du Freifunk mit jedem WLAN-fähigem Gerät nutzen. Sei es Smartphone, Tablett oder PC. Sobald du ein WLAN angezeigt bekommst, welches den Namen flensburg.freifunk.net hat, kannst du dich dort ohne Passwort einloggen. Wo der nächste Knoten steht, siehst du auf unserer Karte.</p>
-	</div>
-
-	<div class="faq_content">
-			<img src="media/questionmark/questionmark.png"alt="">
-
-			<h4>Frage</h4>
-			<p>Was ist wenn jemand etwas illegales über meinen Knoten herrunterlädt?</p>
-			<h4>Antwort</h4>
-			<p>Du musst dir keine Sorgen machen, dass du Post von einem Anwalt bekommst. Wir sorgen mit technischen Mitteln dafür, das alles was über die Freifunk Knoten passiert nicht direkt, sondern über uns ins Internet geht. Der Freifunkverkehr wird verschlüsselt und über unsere Server ins Netz geleitet. So musst du dir keine Sorge um die Störerhaftung machen, falls jemand über deinen Knoten Illegales herrunterlädt.</p>
-	</div>
-
-</div>
-
-<div class="faq_eaven">
-	<div class="faq_content">
-			<img src="media/questionmark/questionmark.png"alt="">
-
-			<h4>Frage</h4>
-			<p>Was kostet Freifunk ?</p>
-			<h4>Antwort</h4>
-			<p>Wenn du Freifunk surfen willst kostet es dich nichts. Wenn Du selbst einen Freifunk-Zugang betreiben willst, entstehen lediglich Kosten für die Anschaffung eines Knoten, welche ab 18 Euro beginnen. Hinzuzüglich kommen Stromkosten, die sich jährlich auf etwa 5 Euro belaufen. Wenn Du zusätzlich auch noch deinen Internetanschluss teilen möchtest kommen natürlich auch noch die Kosten für diesen hinzu.</p>
-	</div>
-
-	<div class="faq_content">
-			<img src="media/questionmark/questionmark.png"alt="">
-
-			<h4>Frage</h4>
-			<p>Ich habe keine Ahnung von Technik. Kann ich trotzdem einen Knoten betreiben?</p>
-			<h4>Antwort</h4>
-			<p>Ja das kannst du. Entweder kannst du dir selber einen Knoten kaufen oder wir kaufen einen für Dich und helfen dir gern bei der Einrichtung oder richten diesen fertig ein, so dass Du deinen Knoten nur noch in die Steckdose stecken musst und gegebenenfalls mit deinem Router verbinden musst.</p>
-	</div>
-
-	<div class="faq_content">
-			<img src="media/questionmark/questionmark.png"alt="">
-
-			<h4>Frage</h4>
-			<p>Ich habe keine Ahnung von Technik. Kann ich trotzdem einen Knoten betreiben?</p>
-			<h4>Antwort</h4>
-			<p>Ja das kannst du. Entweder kannst du dir selber einen Knoten kaufen oder wir kaufen einen für Dich und helfen dir gern bei der Einrichtung oder richten diesen fertig ein, so dass Du deinen Knoten nur noch in die Steckdose stecken musst und gegebenenfalls mit deinem Router verbinden musst. </p>
-	</div>
-</div>
+	<article class="faq_content">
+		<h4>Frage</h4>
+		<p>Ich habe keine Ahnung von Technik. Kann ich trotzdem einen Knoten betreiben?</p>
+		<h4>Antwort</h4>
+		<p>Ja das kannst du. Entweder kannst du dir selber einen Knoten kaufen oder wir kaufen einen für Dich und helfen dir gern bei der Einrichtung oder richten diesen fertig ein, so dass Du deinen Knoten nur noch in die Steckdose stecken musst und gegebenenfalls mit deinem Router verbinden musst. </p>
+	</article>
+</section>
