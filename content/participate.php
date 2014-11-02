d791beea191e22ead2f85ec1b9bf31564215fc66
malteriechmann <kontakt@malteriechmann.de>
2014-11-02 03:56:01 +0100

    typo

--- content/participate.php
+++ content/participate.php
@@ -26,7 +26,7 @@
         <p>Am einfachsten kannst du bei Freifunk Flensburg mitmachen, indem du selber einen Knoten aufstellst. Ein Knoten ist ein technisches Gerät (ein Router), auf dem unsere Freifunk-Software läuft. Den Knoten kannst du dir bei uns für ca. 20 Euro, komplett fertig eingerichtet, kaufen. Deinen Knoten brauchst du dann nur noch an den Strom und an das Internet anzuschließen und schon können du und andere über Freifunk surfen. So einfach ist das.</p>
         <h4>Mach mehr und bringe dich ein</h4>
         <p>Wir treffen uns jeden Montag mit einer Gruppen von Technikinteressierten und besprechen den weiteren Verlauf von Freifunk Flensburg. Komm doch einfach mal vorbei. Wir treffen uns beim Cowork Flensburg, Toosbüystraße 13, immer ab 18:00 Uhr.</p>
-        <h4>War etwas unverstänldich?</h4>
+        <h4>War etwas unverständlich?</h4>
         <p>Dann kontaktiere uns. Wo du uns genau findest, ob online oder offline, findest du <a href="#contact">unter Kontakt</a>.</p>
     </article>
     <article id="member" class="participate_show-customer">
