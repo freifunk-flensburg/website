053f1d03f5a09810bfdb48138df8f055d21bf2f2
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2014-11-02 05:03:00 +0100

    changed gendering

--- content/about.php
+++ content/about.php
@@ -4,6 +4,6 @@
     </figure>
     <h3>Was ist Freifunk Flensburg?</h3>
     <p>Freifunk Flensburg ist dein freies WLAN zum selber machen in Flensburg und Umgebung.</p>
-    <p>Wir möchten möglichst vielen Menschen überall und jederzeit einen guten Zugang zum Internet bieten. Dafür arbeiten wir an einem Gemeinschaftsnetz, welches jede_r vergrößern und verbessern kann.</p>
+    <p>Wir möchten möglichst vielen Menschen überall und jederzeit einen guten Zugang zum Internet bieten. Dafür arbeiten wir an einem Gemeinschaftsnetz, welches jede*r vergrößern und verbessern kann.</p>
     <p>Über Freifunk surfen ist nicht nur kostenlos, sondern auch unbegrenzt, kennwortlos und super einfach.</p>
 </div>
