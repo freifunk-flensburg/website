fe15fb05b39f914696b9fde91f8af47e993ee13c
malteriechmann <kontakt@malteriechmann.de>
2014-11-03 21:30:09 +0100

    changed contact design a bit

--- content/contact.php
+++ content/contact.php
@@ -1,22 +1,18 @@
 <div class="contact_left">
-	<div class="contact_content">
-		<h3>Offline</h3>
-		<p>Freifunk Flensburg ist ein Projekt des nordlab e.V.. Interessierte sind gerne eingeladen bei unseren Treffen am Montag bei Cowork Flensburg, Toosbüystraße 13, ab 18:00 Uhr vorbei zu schauen.</p>
-		<address>
-			Telefon: <a href="tel:046157494595">0461 57494595</a>  <br>
-			Freifunk Flensburg <br>
-			c/o nordlab e.V. <br>
-			Rude 27 <br>
-			24941 Flensburg <br>
-		</address>
-	</div>
+	<h3>Offline</h3>
+	<p>Freifunk Flensburg ist ein Projekt des nordlab e.V.. Interessierte sind gerne eingeladen bei unseren Treffen am Montag bei Cowork Flensburg, Toosbüystraße 13, ab 18:00 Uhr vorbei zu schauen.</p>
+	<address>
+		Telefon: <a href="tel:046157494595">0461 57494595</a>  <br>
+		Freifunk Flensburg <br>
+		c/o nordlab e.V. <br>
+		Rude 27 <br>
+		24941 Flensburg <br>
+	</address>
 </div>
 <div class="contact_right">
-	<div class="contact_content">
-		<h3>Online</h3>
-		E-Mail-Adresse: <a href="mailto:presse@freifunk-flensburg.de">presse@freifunk-flensburg.de</a><br>
-		Facebook: <a href="https://www.facebook.com/FreifunkFlensburg">facebook.com/FreifunkFlensburg</a><br>
-		Twitter: <a href="https://twitter.com/FreiFunkFlens">@FreiFunkFlens</a><br>
-		IRC: <a href="http://wiki.freifunk-flensburg.de/wiki/IRC">#freifunk-flensburg</a>
-	</div>
+	<h3>Online</h3>
+	E-Mail-Adresse: <a href="mailto:presse@freifunk-flensburg.de">presse@freifunk-flensburg.de</a><br>
+	Facebook: <a href="https://www.facebook.com/FreifunkFlensburg">facebook.com/FreifunkFlensburg</a><br>
+	Twitter: <a href="https://twitter.com/FreiFunkFlens">@FreiFunkFlens</a><br>
+	IRC: <a href="http://wiki.freifunk-flensburg.de/wiki/IRC">#freifunk-flensburg</a>
 </div>
