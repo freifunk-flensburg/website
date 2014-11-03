5f2a921676f403e580513c61466801bafe22e763
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2014-11-03 22:06:12 +0100

    added new faq question
    deleted duplicate faq question and fixed a typo

--- content/faq.php
+++ content/faq.php
@@ -25,7 +25,7 @@
 		<h4>Frage</h4>
 		<p>Was kostet Freifunk?</p>
 		<h4>Antwort</h4>
-		<p>Wenn du Freifunk surfen willst kostet es dich nichts. Wenn Du selbst einen Freifunk-Zugang betreiben willst, entstehen lediglich Kosten für die Anschaffung eines Knoten, welche ab 18 Euro beginnen. Hinzuzüglich kommen Stromkosten, die sich jährlich auf etwa 5 Euro belaufen. Wenn Du zusätzlich auch noch deinen Internetanschluss teilen möchtest kommen natürlich auch noch die Kosten für diesen hinzu.</p>
+		<p>Wenn du mit Freifunk surfen willst kostet es dich nichts. Wenn Du selbst einen Freifunk-Zugang betreiben willst, entstehen lediglich Kosten für die Anschaffung eines Knoten, welche ab 18 Euro beginnen. Hinzuzüglich kommen Stromkosten, die sich jährlich auf etwa 5 Euro belaufen. Wenn Du zusätzlich auch noch deinen Internetanschluss teilen möchtest kommen natürlich auch noch die Kosten für diesen hinzu.</p>
 	</article>
 	<article class="faq_content">
 		<h4>Frage</h4>
@@ -36,8 +36,8 @@
 
 	<article class="faq_content">
 		<h4>Frage</h4>
-		<p>Ich habe keine Ahnung von Technik. Kann ich trotzdem einen Knoten betreiben?</p>
+		<p>Wie verbinden sich eure Router und woher kommt dieses neue WLAN mit komischen Namen?</p>
 		<h4>Antwort</h4>
-		<p>Ja das kannst du. Entweder kannst du dir selber einen Knoten kaufen oder wir kaufen einen für Dich und helfen dir gern bei der Einrichtung oder richten diesen fertig ein, so dass Du deinen Knoten nur noch in die Steckdose stecken musst und gegebenenfalls mit deinem Router verbinden musst. </p>
+		<p>Unsere Knoten verbinden sich über ihr WLAN kabellos zu einem Mesh-Netzwerk, das heisst sie sprechen miteinander. So können zwei Knoten Daten austauschen, ihren Internetanschluss teilen und so das Freifunknetz erweitern. Dafür baut jeder Router auch ein Netzwerk mit dem Namen 22:22:54:78:09:43 auf. Dieses kannst du getrost ignorieren.</p>
 	</article>
 </section>
