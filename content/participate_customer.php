b47ce97475130a9af7b30e7f1ae31186f85940f7
René Krick <rene.krick@gmx.de>
2015-01-08 14:24:18 +0100

    ortsangabe berichtigt.

--- content/participate_customer.php
+++ content/participate_customer.php
@@ -3,6 +3,6 @@
 <h4>Der erste Schritt, dein eigener Knoten</h4>
 <p>Am einfachsten kannst du bei Freifunk Flensburg mitmachen, indem du selber einen Knoten aufstellst. Ein Knoten ist ein technisches Gerät (ein Router), auf dem unsere Freifunk-Software läuft. Den Knoten kannst du dir bei uns für ca. 20 Euro, komplett fertig eingerichtet, kaufen. Deinen Knoten brauchst du dann nur noch an den Strom und an das Internet anzuschließen und schon können du und andere über Freifunk surfen. So einfach ist das.</p>
 <h4>Mach mehr und bringe dich ein</h4>
-<p>Wir treffen uns jeden Montag mit einer Gruppen von Technikinteressierten und besprechen den weiteren Verlauf von Freifunk Flensburg. Komm doch einfach mal vorbei. Wir treffen uns beim Cowork Flensburg, Toosbüystraße 13, immer Montags ab 18:00 Uhr.</p>
+<p>Wir treffen uns jeden Montag mit einer Gruppen von Technikinteressierten und besprechen den weiteren Verlauf von Freifunk Flensburg. Komm doch einfach mal vorbei. Wir treffen uns im Offenen Kanal Flensburg, St.-Jürgen-Straße 95, immer Montags ab 18:00 Uhr.</p>
 <h4>War etwas unverständlich?</h4>
 <p>Dann kontaktiere uns. Wo du uns genau findest, ob online oder offline, findest du <a href="#contact">unter Kontakt</a>.</p>
