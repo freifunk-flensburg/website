0cee9bf567a1748ca1b660177570232cc120c22c
Fabian Horst <fabian@fabian-horst.de>
2015-06-17 02:14:25 +0200

    Revert "Fffl 2015"

--- content/contact.php
+++ content/contact.php
@@ -1,11 +1,16 @@
 <div class="contact_left">
 	<h3>Offline</h3>
-	<p>Freifunk Flensburg ist ein Community mit Unterstützung des NordLab e.V. . Interessierte sind gerne eingeladen bei den Treffen des nordlab e.V. Montags im Offenen Kanal, St.-Jürgen-Straße 95, ab 18:00 Uhr vorbei zu schauen. <br></p>
+	<p>Freifunk Flensburg ist ein Projekt des nordlab e.V. und des Chaostreff Flensburg. Interessierte sind gerne eingeladen bei den Treffen des nordlab e.V. Montags im Offenen Kanal, St.-Jürgen-Straße 95, ab 18:00 Uhr vorbei zu schauen. <br>Auch bei den OpenLab Treffen, Dienstags ab 17.30 Uhr im Chaostreff Flensburg, Toosbüystraße 13, könnt ihr euch über Freifunk informieren.</p>
 	<address>
 		Telefon: <a href="tel:046157494595">0461 57494595</a>  <br>
 		nordlab e.V. <br>
 		Rude 27 <br>
 		24941 Flensburg
+	</address>
+	<address>
+		Chaostreff Flensburg <br>
+		Toosbüystraße 13 <br>
+		24939 Flensburg
 	</address>
 </div>
 <div class="contact_right">
@@ -14,8 +19,7 @@
 		E-Mail-Adresse: <a target="_blank" href="mailto:presse@freifunk-flensburg.de">presse@freifunk-flensburg.de</a><br>
 		Facebook: <a target="_blank" href="https://www.facebook.com/FreifunkFlensburg">facebook.com/FreifunkFlensburg</a><br>
 		Twitter: <a target="_blank" href="https://twitter.com/FreiFunkFlens">@FreiFunkFlens</a><br>
-		IRC: <a target="_blank" href="http://wiki.freifunk-flensburg.de/wiki/IRC">#freifunk-flensburg</a><br>
-		Mailingliste: <a target="_blank" href="https://ml06.ispgateway.de/mailman/listinfo/liste_freifunk-flensburg.de">Anmeldung</a>
+		IRC: <a target="_blank" href="http://wiki.freifunk-flensburg.de/wiki/IRC">#freifunk-flensburg</a>
 	</address>
 	<p>Besuche uns im <a href="https://kiwiirc.com/client/irc.lugfl.de:+6668/?nick=Website-Guest|?#freifunk-flensburg" target="_blank">Webchat</a>.</p>
 	<p>Videos von uns findest du auch auf unserem <a href="https://www.youtube.com/channel/UCWX6f-3kFLJkuPwWvnZX3fA/" target="blank">YouTube-Kanal</a>.</p>
