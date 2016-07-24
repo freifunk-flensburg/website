399869bc0aeec190a40fc7a2afa279e1870ed14c
rubo77 <github@r.z11.de>
2016-07-24 11:33:18 +0200

    Remove map jquery call fo fix js Error: Map container not found.

--- assets/javascripts/jquery.main.js
+++ assets/javascripts/jquery.main.js
@@ -20,64 +20,6 @@ $(document).ready(function() {
 });
 
 /*
-    get json data for map
-*/
-$(document).ready(function() {
-    $.ajax({
-      url: 'assets/javascripts/leaflet-0.7.3/leaflet.js',
-      dataType: 'script',
-      cache: true,
-      success: function() {
-        var myIcon = L.icon({
-            iconUrl: 'assets/images/freifunk-flensburg-map-icon.png',
-            iconRetinaUrl: 'assets/images/freifunk-flensburg-map-icon.png',
-            iconSize: [30, 32],
-            iconAnchor: L.Point(30, 32),
-            popupAnchor: L.Point(-15, -32)
-        });
-
-        var map = L.map('map', {
-            icon: myIcon,
-            scrollWheelZoom: false,
-            center: [54.790961, 9.435912],
-            zoom: 13
-        });
-
-        L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        var setNodeToMap = function(node) {
-            if(node.nodeinfo.location && node.flags.online) {
-                L.marker([node.nodeinfo.location.latitude, node.nodeinfo.location.longitude], {icon: myIcon}).addTo(map).bindPopup('<h3>' + node.nodeinfo.hostname + '</h3>');
-            }
-        };
-
-        $.ajax({
-            cache: false,
-            url: 'nodes.json',
-            dataType: 'json',
-            success: function(data) {
-                var countNodes = 0;
-
-                for (var node in data.nodes) {
-                    setNodeToMap(data.nodes[node]);
-
-                    if (data.nodes[node].flags.online && data.nodes[node].nodeinfo.hostname !== '') {
-                        countNodes++;
-                    }
-                }
-
-                $('#count-nodes').html(countNodes);
-            }
-        });
-
-      }
-    });
-
-});
-
-/*
     choose customer
 */
 $(document).ready(function() {
